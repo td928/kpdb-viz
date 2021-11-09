e093357fd8a401e98122d87717b809206360b243
Te Du <tedudevelop@gmail.com>
2021-11-09 12:37:40 -0500

    edit runtime.txt and add server

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from socioeconomic import create_socioeconomic_tab
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 #app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # -- Import and clean data (importing csv into pandas)
